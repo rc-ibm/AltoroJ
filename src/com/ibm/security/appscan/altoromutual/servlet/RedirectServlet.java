6b472f4feeaa1af195e179a6a7209cf08cc084bf
Rachel Jia Ying Chua <Rachel.Chua@ibm.com>
2025-02-19 01:13:36 +0800

    Fixed unvalidated URL forward vulnerability in RedirectServlet by implementing a whitelist of allowed URLs

--- src/com/ibm/security/appscan/altoromutual/servlet/RedirectServlet.java
+++ src/com/ibm/security/appscan/altoromutual/servlet/RedirectServlet.java
@@ -1,23 +1,7 @@
-/**
-This application is for demonstration use only. It contains known application security
-vulnerabilities that were created expressly for demonstrating the functionality of
-application security testing tools. These vulnerabilities may present risks to the
-technical environment in which the application is installed. You must delete and
-uninstall this demonstration application upon completion of the demonstration for
-which it is intended. 
-
-IBM DISCLAIMS ALL LIABILITY OF ANY KIND RESULTING FROM YOUR USE OF THE APPLICATION
-OR YOUR FAILURE TO DELETE THE APPLICATION FROM YOUR ENVIRONMENT UPON COMPLETION OF
-A DEMONSTRATION. IT IS YOUR RESPONSIBILITY TO DETERMINE IF THE PROGRAM IS APPROPRIATE
-OR SAFE FOR YOUR TECHNICAL ENVIRONMENT. NEVER INSTALL THE APPLICATION IN A PRODUCTION
-ENVIRONMENT. YOU ACKNOWLEDGE AND ACCEPT ALL RISKS ASSOCIATED WITH THE USE OF THE APPLICATION.
-
-IBM AltoroJ
-(c) Copyright IBM Corp. 2008, 2013 All Rights Reserved.
- */
 package com.ibm.security.appscan.altoromutual.servlet;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -34,8 +18,15 @@ import javax.servlet.http.HttpServletResponse;
  * @author Alexei
  */
 public class RedirectServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
+    // Define a whitelist of allowed URLs
+    private static final String[] ALLOWED_URLS = {
+        "/login.jsp",
+        "/home.jsp",
+        "/about.jsp"
+    };
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -43,15 +34,27 @@ public class RedirectServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = request.getServletPath().toString();
-		if (url.endsWith(".aspx")) {
-			url = url.substring(0, url.lastIndexOf(".aspx")) + ".jsp";
-		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getServletPath().toString();
+        if (url.endsWith(".aspx")) {
+            url = url.substring(0, url.lastIndexOf(".aspx")) + ".jsp";
+        }
+
+        // Check if the URL is in the whitelist
+        if (isAllowedUrl(url)) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        } else {
+            // Handle the case where the URL is not allowed
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "URL not found");
+        }
+    }
+
+    // Method to check if a URL is in the whitelist
+    private boolean isAllowedUrl(String url) {
+        return Arrays.stream(ALLOWED_URLS).anyMatch(allowedUrl -> allowedUrl.equals(url));
+    }
 }
