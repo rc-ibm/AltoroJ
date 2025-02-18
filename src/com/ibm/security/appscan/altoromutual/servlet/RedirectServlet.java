7f1a20d21a3ddfc3ed60d972c087030574768bc2
Rachel Jia Ying Chua <Rachel.Chua@ibm.com>
2025-02-18 22:44:09 +0800

    Fixed unvalidated URL forward vulnerability in RedirectServlet

--- src/com/ibm/security/appscan/altoromutual/servlet/RedirectServlet.java
+++ src/com/ibm/security/appscan/altoromutual/servlet/RedirectServlet.java
@@ -1,23 +1,8 @@
-/**
-This application is for demonstration use only. It contains known application security
-vulnerabilities that were created expressly for demonstrating the functionality of
-application security testing tools. These vulnerabilities may present risks to the
-technical environment in which the application is installed. You must delete and
-uninstall this demonstration application upon completion of the demonstration for
-which it is intended. 
-
-IBM DISCLAIMS ALL LIABILITY OF ANY KIND RESULTING FROM YOUR USE OF THE APPLICATION
-OR YOUR FAILURE TO DELETE THE APPLICATION FROM YOUR ENVIRONMENT UPON COMPLETION OF
-A DEMONSTRATION. IT IS YOUR RESPONSIBILITY TO DETERMINE IF THE PROGRAM IS APPROPRIATE
-OR SAFE FOR YOUR TECHNICAL ENVIRONMENT. NEVER INSTALL THE APPLICATION IN A PRODUCTION
-ENVIRONMENT. YOU ACKNOWLEDGE AND ACCEPT ALL RISKS ASSOCIATED WITH THE USE OF THE APPLICATION.
-
-IBM AltoroJ
-(c) Copyright IBM Corp. 2008, 2013 All Rights Reserved.
- */
 package com.ibm.security.appscan.altoromutual.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -34,8 +19,16 @@ import javax.servlet.http.HttpServletResponse;
  * @author Alexei
  */
 public class RedirectServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+    private static final List<String> VALID_URLS = new ArrayList<>();
+
+    static {
+        // Initialize the list of valid URLs
+        VALID_URLS.add("/home.jsp");
+        VALID_URLS.add("/about.jsp");
+        // Add more valid URLs as needed
+    }
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -43,15 +36,26 @@ public class RedirectServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = request.getServletPath().toString();
-		if (url.endsWith(".aspx")) {
-			url = url.substring(0, url.lastIndexOf(".aspx")) + ".jsp";
-		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getServletPath().toString();
+        if (url.endsWith(".aspx")) {
+            url = url.substring(0, url.lastIndexOf(".aspx")) + ".jsp";
+        }
+        
+        // Check if the requested URL is in the list of valid URLs
+        if (isValidUrl(url)) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        } else {
+            // Handle invalid URLs, e.g., by sending an error page
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid URL");
+        }
+    }
+
+    private boolean isValidUrl(String url) {
+        return VALID_URLS.contains(url);
+    }
 }
